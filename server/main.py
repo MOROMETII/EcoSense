f526e32a564f41116c03c5a9c6a83a886401f4cf
IleaBogdan <ilea.bogdan06@gmail.com>
2026-04-25 03:30:30 +0300

    fixed users.sql

--- server/main.py
+++ server/main.py
@@ -43,4 +43,5 @@ def index():
     return render_template("index.html")
 
 if __name__=="__main__":
+    get_db()
     app.run(debug=True, port=6969)
--- server/sqls/users.sql
+++ server/sqls/users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
-	ID int AUTO_INCREMENT PRIMARY KEY,
-	Username varchar(50) NOT NULL,
-	Password varchar(255) NOT NULL,
-	Mail varchar(50) NOT NULL
+    ID int NOT NULL AUTO_INCREMENT,
+    Username varchar(50) NOT NULL,
+    Password varchar(255) NOT NULL,
+    Mail varchar(50) NOT NULL,
+    PRIMARY KEY (ID)
 );
